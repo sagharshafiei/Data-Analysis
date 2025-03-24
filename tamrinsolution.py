9df3a310a0e96ec763d34b0fe7823b18f2b7ee16
sagharshafiei <shafieisaghar2@gmail.com>
2025-03-24 13:17:02 +0330

    Add files via upload

---
+++ tamrinsolution.py
@@ -0,0 +1,48 @@
+import yfinance as yf
+import numpy as np
+from matplotlib import pyplot as plt
+from persiantools.jdatetime import JalaliDate
+
+def week_day(btc_date):
+    return JalaliDate(btc_date).isoweekday()
+
+def test(data):
+    print(data)
+    return data
+
+df = yf.download("BTC-USD", start="2020-01-01", end="2024-12-31")
+df["Date"] = df.index
+
+df["WeekDay"] = df["Date"].apply(week_day)
+
+day_filter = (df["WeekDay"] == 6) | (df["WeekDay"] == 2)
+filtered_df = df[day_filter]
+
+filtered_df["Close_5"] = filtered_df["Close"].shift(-1)
+filtered_df.dropna(inplace=True)
+
+new_df = filtered_df[["Open", "Close_5"]]
+
+# new_df.apply(test)
+#
+# print(new_df)
+# df.drop(columns=["Close_5"], inplace=True)
+
+amount = 1000
+amount_list = []
+
+for i in range(len(new_df)):
+    amount_list.append(amount- 1000)
+    btc_amount = amount /  new_df["Open"].iloc[i]
+    amount = btc_amount * new_df["Close_5"].iloc[i]
+    print(amount)
+
+
+
+plt.plot(amount_list)
+plt.show()
+
+print(new_df["Open"].iloc[0], new_df["Open"].iloc[-1])
+
+# کدام روز برای خرید و کدام روز برای فروش مناسب تر است
+# معیار شما چیست؟
